1d22e0141c5d1ff6af000a06b5db4f4fff6a8b28
Emma Nøjsen <emmanojsen@gmail.com>
2023-11-08 14:32:22 +0100

    console.log

---
+++ script.js
@@ -0,0 +1,36 @@
+        // Globalt dataarray
+        let albumData;
+        let albumNameArray = [];
+
+        // set the dimensions and margins of the graph
+        const margin = { top: 20, right: 30, bottom: 40, left: 20 };
+        const width = 700;
+        const height = 500;
+
+ 
+        // loader json data ind
+ d3.json("albums.json").then(function(data) {
+    //Jeg bruger loopet til at dele data op i flere arrays i stedet for et stort objekt
+    for (let i = 0; i < data.length; i++){
+        currentAlbum = data[i];
+
+        //Her laver jeg en en array med alle album navne
+        let albumName = currentAlbum.albumName;
+        albumNameArray.push(albumName);
+        console.log(albumName);
+    };
+
+    /*
+    albumData = data; // Gem data globalt
+    console.log(data.artistName);
+    showDataOnScreenFromJSON(data);
+
+    Create an array of objects from the data and pass that to the function in order to display 
+    custom info not readily available in the JSON data
+  
+    let cds = createCDObjects(data);
+    console.log(cds);
+    showDataOnScreenFromOBJ(cds);
+    */
+  });
+  
